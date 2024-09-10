218182ad0c0e54e488532fddd6c76a9f0f930bec
Yahya Sodki <proyahya2022@gmail.com>
2024-09-10 14:30:59 +0100

    Create a middelware function and function to handle possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.moodel.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup  = async (req , res)=>{
+export const signup  = async (req , res , next)=>{
     const {username , email , password} = req.body;
     const hashPassword = bcryptjs.hashSync(password , 10);
 
@@ -11,6 +12,6 @@ export const signup  = async (req , res)=>{
         await newUser.save();
         res.status(201).json("User created successfully !");
     }catch(error){
-        res.status(500).json({"error" : error.errorResponse.errmsg});
+        next(error);
     }
 };
--- api/index.js
+++ api/index.js
@@ -20,3 +20,13 @@ app.use("/api/auth" , authRouter);
 app.listen(port , ()=>{
     console.log(`Server is runing on port ${port}`);
 });
+
+app.use((err , req , res , next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success : false ,
+        statusCode ,
+        message
+    });
+});
