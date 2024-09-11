b801a6ac36be7ceb7461f2441215ff79e406391f
Yahya Sodki <proyahya2022@gmail.com>
2024-09-11 11:02:00 +0100

    Add signin functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup , signin} from "../controllers/auth.controller.js";
 const authRouter = express.Router();
 
 authRouter.post("/signup" , signup);
-
+authRouter.post("/signin" , signin);
 export default authRouter;
